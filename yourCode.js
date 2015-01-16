5d0f17a7a821205c906f3c6375b216ef5edee230
Noah Thederahn <thederahnn16@hanover.edu>
2015-01-16 15:35:11 -0500

    Issue #6 introduced

--- yourCode.js
+++ yourCode.js
@@ -8,7 +8,15 @@
  */
 var binarySearch = function binarySearch(arr, val) {
    var lo, hi, mid;
-
+   if arr.length === 0 {
+      return false;
+   }
+   lo = arr[0];
+   hi = arr[arr.length-1];
+   if val > hi || val < lo {
+      return false;
+   }
+   mid = arr[Math.floor(arr.lengh/2)];
    // You may need to add things here
 
    while (false) {     // You should change this with a proper condition
