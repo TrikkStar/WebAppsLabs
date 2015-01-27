a1c01148c9e05b33cae617522d57c4cdd5fbbfed
Noah Thederahn <thederahnn16@hanover.edu>
2015-01-27 15:53:24 -0500

    added most testing for makeTaskFromObject (#51)

--- task.spec.js
+++ task.spec.js
@@ -9,6 +9,16 @@ expect = require('./chai.js').expect;
 
 Task = require('./task.js');
 
+function randomString(len) {
+	var arr = [], caseRange, i;
+	if (len == null) { len = 5; }
+	for (i = 0; i < len; i += 1) {
+		caseRange = [65, 97][Math.floor(Math.random() * 2)];
+		arr.push(Math.floor(Math.random() * 26) + caseRange);
+	}
+	return String.fromCharCode.apply(String, arr);
+}
+
 // ADD YOUR TESTS HERE
 describe('Your makeNewTask function', function() {
 	var task = Task.new();
@@ -18,4 +28,25 @@ describe('Your makeNewTask function', function() {
 	it('returns an object with correct keys', function(){
 		expect(task).to.have.ownProperty('title', 'completedTime', 'tags', 'id');
 	});
+});
+
+describe('Your makeTaskFromObject function', function() {
+	var obj, task, str;
+	str = randomString(5);
+	obj = {'title': str, 'id': 'ToBeDetermined'};
+	task = Task.fromObject(obj);
+
+	it('returns an object', function(){
+		expect(task).to.be.a('object');
+	});
+	it('returns an object with correct keys', function(){
+		expect(task).to.have.ownProperty('title', 'completedTime', 'tags', 'id');
+	});
+	it('returns an object with correct title value', function(){
+		expect(task.title).to.equal(str);
+	});
+	// it('returns an object with correct tags', function(){
+	// 	//unsure how to impliment this test, need to do later. 
+	// 	//Possibly in a diffrent testing case
+	// });
 });
