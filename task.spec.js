08d329bf8e3f8aacdc09a189e90832ff982cbbe4
Shelby Babcock <babcocks15@hanover.edu>
2015-01-28 18:32:41 -0500

    close #53 close #54

--- task.spec.js
+++ task.spec.js
@@ -54,3 +54,28 @@ describe('Your makeTaskFromObject function', function() {
 		expect(task.tags).to.include(t2);
 	});
 });
+
+describe('Your makeTaskFromString function', function(){
+	var str, task, t1, t2, full;
+	str = randomString(7);
+	t1 = "CharliePlaysPokemon";
+	t2 = "abc";
+	full = str + "#" + t1 + "#" + t2;
+	console.log("full: ", full);
+	task = Task.fromString(full);
+
+	it('returns an object', function(){
+		expect(task).to.be.a('object');
+	});
+	it('returns an object with correct keys', function(){
+		expect(task).to.have.ownProperty('title', 'completedTime', 'tags', 'id');
+	});
+	it('returns an object with the correct title value', function(){
+		expect(task.title).to.equal(str);
+	});
+	it('returns an object with correct tags',function(){
+		expect(task.tags).to.have.length(2);
+		expect(task.tags).to.include(t1);
+		expect(task.tags).to.include(t2);
+	});
+})
