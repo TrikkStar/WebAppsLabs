5d38372c2178928f86031463953d6514409a5d85
Noah Thederahn <thederahnn16@hanover.edu>
2015-02-02 10:11:13 -0500

    formatting and syntax corrections

--- collection.js
+++ collection.js
@@ -36,9 +36,9 @@ function makePredicateFromArg(arg){
 function makeFunctionFromArg(arg){
     if (typeof arg === "function"){
         return arg;
-    } else if (typef arg === "number"){
+    } else if (typeof arg === "number"){
         return function(task){ return task.id === arg; };
-    } else if (typef arg === "string"){
+    } else if (typeof arg === "string"){
         return function(task){ return task.title === arg; };
     }
     // need to determine if a regular expression can be evaluated the same as a string or not
@@ -56,51 +56,51 @@ function getIndex(arg){
 
 proto = {
    length: function length(){
-    "use strict";
-    return this.values.length();
+		"use strict";
+		return this.values.length();
    },
    isEmpty: function isEmpty(){
-    "use strict";
-    return this.length() !== 0;
+		"use strict";
+		return this.length() !== 0;
    },
    get: function get(arg){
-    "use strict";
-    var x = getIndex(arg);
-    if (x === -1){
-        return null;
-    }
-    return this.values[x];
+		"use strict";
+		var x = getIndex(arg);
+		if (x === -1){
+		    return null;
+		}
+		return this.values[x];
    },
    has: function has(arg){
-    "use strict";
-    if (getIndex(arg) === -1){
-        return false;
-    }
-    return true;
+		"use strict";
+		if (getIndex(arg) === -1){
+			return false;
+		}
+		return true;
    },
    // arg is either a task or an array of tasks.  Make a addOneTask function.
    addOneTask: function addOneTask(task){
-    "use strict";
+    	"use strict";
    },
    add: function add(arg){
-    "use strict";
+    	"use strict";
    },
    new: function newTask(){
-    "use strict";
-    var task = Task.new();
-    this.add(task);
-    return task;
+	    "use strict";
+	    var task = Task.new();
+	    this.addOneTask(task);
+	    return task;
    },
    // arg is either one number or an array of numbers.
    remove: function remove(arg){
-    "use strict";
+    	"use strict";
    },
    // arg can be a function, an array of numbers, a string, or a regular expression.
    filter: function filter(arg){
-    "use strict";
+    	"use strict";
    },
-   forEach: function forEach(f){
-    "use strict";
+   forEach: function forEach(func){
+    	"use strict";
    }
 };
 
