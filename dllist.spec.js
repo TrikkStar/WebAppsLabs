09f4e688f87f76f495ecc59260fe82e9604516ca
Noah Thederahn <thederahnn16@hanover.edu>
2015-03-10 15:19:20 -0400

    added initial testing and shell for prototype methods

--- dllist.spec.js
+++ dllist.spec.js
@@ -10,4 +10,13 @@ expect = require('./chai.js').expect;
 DLList = require('./dllist.js');
 CmdHistory = require('./history.js');
 
-// ADD YOUR TESTS HERE
+describe('Your makeNewList function', function(){
+	var list;
+	list = DLList.new();
+	it('returns an object', function(){
+		expect(list).to.be.a("object");
+	});
+	it('returns an object with correct keys', function(){
+		expect(list).to.have.ownProperty("sentinel", "isEmpty", "length", "first", "last", "insertAt", "unshift", "push", "endAt", "remove", "pop", "shift", "isFirst", "isLast", "iterator", "forEach", "toArray", "iterateFrom", "reverseIterateFrom");
+	});
+});
