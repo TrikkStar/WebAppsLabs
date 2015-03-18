0b28dbfe2ac3e15c82f6ac6ef5ffb62684d9990d
Noah Thederahn <thederahnn16@hanover.edu>
2015-03-18 16:12:30 -0400

    implimented add (Close #123)

--- history.js
+++ history.js
@@ -26,7 +26,12 @@ function makeNewHistory() {
 
 proto = {
 	add: function(cmd){
-
+		if (this.current === null){
+			this.current = this.list.push(cmd);
+		} else {
+			this.current = this.list.insertAt(cmd, this.current);
+		}
+		cmd.execute();
 	},
 	canRedo: function(){
 		if (this.current !== null){
@@ -35,7 +40,10 @@ proto = {
 		return false;
 	},
 	canUndo: function(){
-
+		if (this.current !== null){
+			return !this.list.isFirst(this.current);
+		}
+		return false;
 	},
 	redo: function(){
 
