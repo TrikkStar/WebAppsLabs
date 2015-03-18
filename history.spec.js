4577ee6c2766f9fe1abb9daa6470e1614868557d
Noah Thederahn <thederahnn16@hanover.edu>
2015-03-18 19:31:32 -0400

    fully tested history.js (Close #130)

--- history.spec.js
+++ history.spec.js
@@ -48,6 +48,10 @@ describe("Your CmdHistory object", function(){
 		expect(hist.canUndo()).to.be.true;
 		expect(hist.canRedo()).to.be.false;
 		expect(LogEntries[0]).to.equal("command 1 executed");
+		hist.undo();
+		hist.add(mockCommand());
+		expect(hist.current.value).to.not.equal(cmd);
+		expect(LogEntries[2]).to.equal("command 2 executed");
 	});
 	it("undo's commands properly", function(){
 		var hist, cmd;
@@ -66,5 +70,45 @@ describe("Your CmdHistory object", function(){
 		expect(hist.canUndo()).to.be.false;
 		expect(hist.canRedo()).to.be.true;
 		expect(function() {hist.undo()}).to.throw(Error);
+	});
+	it("redo's commands properly", function(){
+		var hist, cmd;
+		hist = CmdHistory.new();
+		cmd = mockCommand();
+		hist.add(mockCommand());
+		hist.add(cmd);
+		hist.add(mockCommand());
+		expect(hist.current.value).to.not.equal(cmd);
+		hist.undo();
+		expect(hist.current.value).to.equal(cmd);
+		expect(LogEntries[3]).to.equal("command 3 unexecuted");
+		expect(function() {hist.redo()}).to.not.throw(Error);
+		expect(hist.current.value).to.not.equal(cmd);
+		expect(LogEntries[4]).to.equal("command 3 executed");
+		expect(function() {hist.redo()}).to.throw(Error);
+	});
+	it("gives proper iterators", function(){
+		var hist, cmd, i, ittrU, ittrR;
+		hist = CmdHistory.new();
+		for (i = 0; i < 4; i += 1){
+			hist.add(mockCommand());
+		}
+		cmd = mockCommand();
+		hist.add(cmd);
+		for (i = 0; i < 4; i += 1){
+			hist.add(mockCommand());
+		}
+		for (i = 0; i < 4; i += 1){
+			hist.undo();
+		}
+		expect(hist.current.value).to.equal(cmd);
+		ittrR = hist.redoableIterator().toArray();
+		ittrU = hist.undoableIterator().toArray();
+		expect(ittrR.length).to.equal(4);
+		expect(ittrU.length).to.equal(5);
+		expect(ittrR[0].value.toString).to.equal("command 6");
+		expect(ittrU[0].value.toString).to.equal(cmd.toString);
+		expect(ittrR[3].value.toString).to.equal("command 9");
+		expect(ittrU[4].value.toString).to.equal("command 1");
 	});
 });
